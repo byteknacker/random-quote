e75afd18a01a0cc8c4c143bfce1fa71ee22d77ce
John Wu <xiangyu.wu.10@gmail.com>
2016-06-21 03:33:21 -0700

    Random quoting is finished

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
     $("#getMessage").on("click", function() {
-        $.getJSON("https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en", function (json) {
-            $("#quote-text").html(JSON.stringify(json));
+        var randomNumber = Math.floor(Math.random() * (40151 - 2 + 1)) + 2;
+        var requestString = "https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en&key=";
+        requestString += randomNumber;
+        $.getJSON(requestString, function (json) {
+            var html = "<p class = 'quote-p'>";
+            html += json["quoteText"];
+            html += "</p>"
+            html += "<footer>Someone famous in <cite title='Source Title'>Source Title</cite></footer>"
+            $("#quote-text").html(html);
         });
     });
 });
