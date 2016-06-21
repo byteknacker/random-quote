4552aa98baf4a0db4153b27fd0a223c3d5395938
John Wu <xiangyu.wu.10@gmail.com>
2016-06-21 02:06:17 -0700

    Add API access to jQuery script: test on Heroku

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("#getMessage").on("click", function() {
+        $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en", function (json) {
+            $("#quote-text").html(JSON.stringify(json));
+        });
+    });
+});
