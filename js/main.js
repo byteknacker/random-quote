848a8531062750179564434a2bf9752e5a24dab4
John Wu <xiangyu.wu.10@gmail.com>
2016-06-21 02:23:44 -0700

    Add API HTTPS proxy so Heroku API access will work

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $("#getMessage").on("click", function() {
-        $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en", function (json) {
+        $.getJSON("https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en", function (json) {
             $("#quote-text").html(JSON.stringify(json));
         });
     });
